aa1ba4132f5aeed7a061046c9ef9ee2b00fe856c
LilBroCodes <lilbrocodes@gmail.com>
2025-11-14 19:30:48 +0100

    Use Composer's scroll events

--- src/main/java/org/lilbrocodes/theatrical/client/TheatricalClient.java
+++ src/main/java/org/lilbrocodes/theatrical/client/TheatricalClient.java
@@ -9,6 +9,7 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import org.lilbrocodes.composer_reloaded.api.controls.BindManager;
 import org.lilbrocodes.composer_reloaded.api.toast.ToastManager;
 import org.lilbrocodes.composer_reloaded.client.duped_binds.BindTracker;
 import org.lilbrocodes.theatrical.Theatrical;
@@ -45,73 +46,16 @@ public class TheatricalClient implements ClientModInitializer {
             "category.theatrical"
     );
 
-    private void countdownFeedback(List<Integer> state) {
-        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-        state.forEach(buf::writeInt);
-        ClientPlayNetworking.send(Theatrical.COUNTDOWN_RESPONSE_PACKET, buf);
-    }
-
     @Override
     public void onInitializeClient() {
         registerBind(HAND_HOLD_BIND);
         registerBind(WALK_SPEED_TRIGGER);
         registerBind(WALK_SPEED_MODIFIER);
-
-
-        ClientPlayNetworking.registerGlobalReceiver(Theatrical.COUNTDOWN_PING_PACKET, (client, handler, buf, sender) -> {
-           List<Integer> checksPassed = new ArrayList<>(List.of(1));
-
-           if (client.world != null && client.player != null) {
-               CountdownInstanceHolderComponent holder = TheatricalCardinalComponents.COUNTDOWNS.get(client.world);
-
-               for (CountdownInstance instance : holder.getHolder()) {
-                   if (instance.players.contains(client.player.getUuid())) {
-                       checksPassed.set(0, 0);
-                   }
-               }
-           }
-
-           checksPassed.addAll(CooldownChecks.runChecks());
-           countdownFeedback(checksPassed);
-        });
-
-        ClientPlayNetworking.registerGlobalReceiver(Theatrical.START_COUNTDOWN_PACKET, (client, handler, buf, sender) -> {
-            CooldownChecks.fixChecks();
-            ToastManager.getInstance().addToast(new CountdownToast(buf.readInt()), ToastManager.Corner.TOP_RIGHT);
-        });
-
-        ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                 HandHeldDataHolder holder = (HandHeldDataHolder) client.player;
-
-                if (HAND_HOLD_BIND.isPressed()) {
-                    if (HAND_HOLD_BIND.wasPressed() && !holder.theatrical$isHandHeldOut()) {
-                        Entity targetEntity = client.targetedEntity;
-                        if (targetEntity instanceof PlayerEntity targetPlayer) {
-                            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-                            buf.writeInt(targetPlayer.getId());
-                            ClientPlayNetworking.send(Theatrical.TAKE_ITEM_PACKET, buf);
-                        } else {
-                            PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-                            buf.writeBoolean(true);
-                            ClientPlayNetworking.send(Theatrical.HAND_HELD_PACKET, buf);
-                        }
-                    }
-                } else {
-                    if (holder.theatrical$isHandHeldOut()) {
-                        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-                        buf.writeBoolean(false);
-                        ClientPlayNetworking.send(Theatrical.HAND_HELD_PACKET, buf);
-                    }
-                }
-            }
-        });
-
-        WorldRenderEvents.AFTER_ENTITIES.register(new SceneVisualizer());
+        TheatricalClientEvents.initialize();
     }
 
     public static void registerBind(KeyBinding keyBinding) {
         KeyBindingHelper.registerKeyBinding(keyBinding);
-        BindTracker.MC_CM_BINDS.add(keyBinding);
+        BindManager.addDuplicateAllowedKeybind(keyBinding);
     }
 }
--- src/main/java/org/lilbrocodes/theatrical/client/events/ScrollEvent.java
+++ src/main/java/org/lilbrocodes/theatrical/client/events/ScrollEvent.java
@@ -1,28 +1,24 @@
-package org.lilbrocodes.theatrical.mixin.impl;
+package org.lilbrocodes.theatrical.client.events;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
+import org.jetbrains.annotations.Nullable;
+import org.lilbrocodes.composer_reloaded.api.controls.scroll.ScrollEvents.ScrollAction;
 import org.lilbrocodes.theatrical.client.TheatricalClient;
 import org.lilbrocodes.theatrical.config.Configs;
 import org.lilbrocodes.theatrical.mixin.accessor.WalkSpeedHolder;
 import org.lilbrocodes.theatrical.util.Misc;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerInventory.class)
-public class PlayerInventoryMixin {
-    @Inject(method = "scrollInHotbar", at = @At("HEAD"), cancellable = true)
-    public void theatrical$changeWalkSpeed(double scrollAmount, CallbackInfo ci) {
+public class ScrollEvent implements ScrollAction {
+    @Override
+    public boolean onScroll(MinecraftClient minecraftClient, @Nullable ClientWorld clientWorld, @Nullable ClientPlayerEntity clientPlayerEntity, double v) {
         if (TheatricalClient.WALK_SPEED_TRIGGER.isPressed() && MinecraftClient.getInstance().player instanceof WalkSpeedHolder walkSpeedHolder) {
-            ci.cancel();
-
-            int i = (int) Math.signum(scrollAmount);
+            int i = (int) Math.signum(v);
             int value = TheatricalClient.WALK_SPEED_MODIFIER.isPressed() ? 5 : 1;
             walkSpeedHolder.theatrical$setWalkSpeed(MathHelper.clamp(walkSpeedHolder.theatrical$getWalkSpeed() + (i * value), 0, 100));
 
@@ -40,6 +36,9 @@ public class PlayerInventoryMixin {
                         walkSpeedHolder.theatrical$getWalkSpeed() / 100.0F * 2.0F
                 );
             }
+
+            return true;
         }
+        return false;
     }
 }
